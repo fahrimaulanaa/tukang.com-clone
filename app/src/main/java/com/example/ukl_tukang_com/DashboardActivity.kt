187178000b88c5b9ff519759f7990085a59b82d6
Fahri Maulana Al Ghazali <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2024-05-07 04:24:38 +0700

    initial commit

---
+++ app/src/main/java/com/example/ukl_tukang_com/DashboardActivity.kt
@@ -0,0 +1,17 @@
+package com.example.ukl_tukang_com
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+    }
+}
